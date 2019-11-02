a4ad3e5ac5c3571aae6037dcc8b9f4bf03dc3636
DavidLejeune <Lejeune.David@outlook.com>
2019-11-02 19:10:35 +0100

    myapp

--- serversetup.sh
+++ serversetup.sh
@@ -27,6 +27,7 @@ echo ""
 echo ">>> ruby install <<<"
 if hash ruby 2>/dev/null; then
     echo "ruby already installed"
+    ruby -v
 else
     rvm install ruby-2.6.3
     echo "> using default 2.6.3 ruby version"
@@ -42,8 +43,10 @@ echo ""
 echo ">>> bundler install <<<"
 if hash bundler 2>/dev/null; then
     echo "bundler already installed"
+    bundle --version
 else
     gem install bundler
+    bundle --version
 fi
 sleep 1
 #read -p "Press enter to continue"
@@ -54,8 +57,10 @@ echo ""
 echo ">>> rails install <<<"
 if hash rails 2>/dev/null; then
     echo "rails already installed"
+    rails -v
 else
     gem install rails
+    echo "> show version"
     rails -v
 fi
 sleep 1
@@ -64,10 +69,11 @@ echo ""
 
 
 echo ">>> creating user for rails application <<<"
-user='gingerbird'
+user='myappuser'
 echo "> testing user existence"
 if [ $(getent passwd $user) ] ; then
         echo user $user exists
+        id $user
 else
         echo user $user doesn\'t exists
         echo "> creating user"
@@ -75,12 +81,54 @@ else
         if [ $(getent passwd $user) ] ; then
             echo user $user exists now !
         fi
+        usermod -aG sudo $user
+        echo "> copying HOME ssh key to here"
+        sudo mkdir -p /home/$user/.ssh
+        touch $HOME/.ssh/authorized_keys
+        sudo sh -c "cat $HOME/.ssh/authorized_keys >> /home/$user/.ssh/authorized_keys"
+        sudo chown -R $user: /home/$user/.ssh
+        sudo chmod 700 /home/$user/.ssh
+        sudo sh -c "chmod 600 /home/$user/.ssh/*"
+
+
+fi
+sleep 1
+#read -p "Press enter to continue"
+echo ""
+
+echo ">>> yarn install <<<"
+if hash yarn 2>/dev/null; then
+    echo "yarn already installed"
+    yarn --version
+else
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt update
+    sudo apt install yarn
+    echo "> show version"
+    yarn --version
 fi
 sleep 1
 #read -p "Press enter to continue"
 echo ""
 
 
+echo ">>> pulling the code <<<"
+myapp='myapp'
+DIR=/var/www/$myapp
+repo='git@github.com:DavidLejeune/ilafirouzabadi.git'
+if [ -d "$DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "${DIR} Already exists ..."
+else
+    sudo mkdir -p /var/www/$myapp
+    sudo chown $user: /var/www/$myapp
+    cd $DIR
+    sudo -u $user -H git clone $repo code
+fi
+sleep 1
+#read -p "Press enter to continue"
+echo ""
 
 
 
